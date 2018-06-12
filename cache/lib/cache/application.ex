ed03144337fb30efb5c09c78c919a0830e96f3fa
ming <chakming@users.noreply.github.com>
2018-06-12 18:03:24 +0800

    🚧 test supervisor with one for one strategy

--- cache/lib/cache/application.ex
+++ cache/lib/cache/application.ex
@@ -2,6 +2,18 @@ defmodule Cache.Application do
   use Application
 
   def start(_type, _args) do
-    Cache.Agent.start_link()
+
+    import Supervisor.Spec
+
+    children = [
+      worker(Cache.Agent, [])
+    ]
+
+    options = [
+      name: Cache.Supervisor,
+      strategy: :one_for_one,
+    ]
+
+    Supervisor.start_link(children, options)
   end
 end
